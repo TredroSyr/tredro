028f42858ffe609037d2e5bd2b63c337169ed08f
safwatbilal <safwetbilal65@gmail.com>
2026-08-28 21:56:42 +0300

    fix next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  allowedDevOrigins: ["192.168.1.*"],
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
